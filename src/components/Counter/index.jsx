146d5a7b0e1c0595433b1be33b9fbc3d5b3fb1d3
MaxBerezhnoi <mohunt.leo@gmail.com>
2022-09-22 20:32:54 +0300

    часть 2

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -5,7 +5,9 @@ class Counter extends React.Component {
     state = {
         good: 0,
         neutral: 0,
-        bad: 0
+        bad: 0,
+        total: 0,
+        positiveFeedback: 0,
     };
     
     onGood = () => {
@@ -13,6 +15,7 @@ class Counter extends React.Component {
         this.setState(prevState => {
             return { good: prevState.good + 1 }
         })
+        this.countTotalFeedback();
     };
     
     onNeutral = () => {
@@ -20,6 +23,7 @@ class Counter extends React.Component {
         this.setState(prevState => {
             return { neutral: prevState.neutral + 1 }
         })
+        this.countTotalFeedback();
     };
     
     onBad = () => {
@@ -27,9 +31,22 @@ class Counter extends React.Component {
         this.setState(prevState => {
             return { bad: prevState.bad + 1 }
         })
+        this.countTotalFeedback();
     };
     
-       
+    countTotalFeedback = () => {
+        this.setState(prevState => {
+            return { total: prevState.good + prevState.neutral + prevState.bad }
+        })
+        this.countPositiveFeedbackPercentage();
+    };
+
+    countPositiveFeedbackPercentage = () => {
+        this.setState(prevState => {
+    return { positiveFeedback: ((prevState.good/prevState.total)*100)}
+})
+    }
+      
     
     render() {
       return (
@@ -65,7 +82,9 @@ class Counter extends React.Component {
         <ul>
                       <li>Good: { this.state.good }</li>
                       <li>Neutral: { this.state.neutral }</li>
-                      <li>Bad: { this.state.bad}</li>
+                  <li>Bad: {this.state.bad}</li>
+                  <li>Total: {this.state.total}</li>
+                  <li>Positive feedback: { this.state.positiveFeedback }%</li>
         </ul>
       </div>
         
